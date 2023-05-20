02f9df269261196c5025483ff476802ebe5a0192
Riyan Fanani <ogickarecka1108@gmail.com>
2023-05-20 08:08:25 +0700

    add chart for statsmodels library

--- test/statsmodelskeks.py
+++ test/statsmodelskeks.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('dataset.csv')
+data = pd.read_csv('data.csv')
 
 # Define the MSAR model
-model = sm.tsa.MarkovAutoregression(df['value'], k_regimes=2, order=1)
+model = sm.tsa.MarkovAutoregression(data['ROA'], k_regimes=2, order=1)
 
 # Estimate the model parameters
 model_result = model.fit()
@@ -16,3 +17,15 @@ predictions = model_result.predict()
 # Analyze the model results
 print(predictions)
 print(model_result.summary())
+print(model_result.filtered_marginal_probabilities[0])
+
+# Show Chart
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.plot(data['ROA'], label='Tahun-Bulan')
+ax.plot(model_result.filtered_marginal_probabilities[0], label='Regime 1')
+ax.plot(predictions, label='Predictions')
+ax.legend()
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.title('Markov Switching Autoregressive (MSAR) Model')
+plt.show()
